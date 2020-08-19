38ec6b854d0ccfe7053abc1e83b93ce95c9cf130
hu <895756412@qq.com>
2020-08-19 21:18:59 +0800

    判断是否有环

---
+++ LinkedList/LinkedListCycle_141.java
@@ -0,0 +1,17 @@
+package Lists;
+
+/**
+ * 判断链表中是否有环
+ */
+public class LinkedListCycle_141 {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while (fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast) return true;
+        }
+        return false;
+    }
+}
