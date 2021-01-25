3177c1d0580dcd273c4025b5533fe2fceb4e935b
hu <895756412@qq.com>
2021-01-25 15:50:57 +0800

    判断是否为二叉排序树

---
+++ Tree/ValidateBinarySearchTree_98.java
@@ -0,0 +1,33 @@
+
+
+
+/**
+ * 判断是否为二叉排序树，每次记录前一个数，判断当前是否大于，如果不是，则不为二叉排序树
+ */
+public class ValidateBinarySearchTree_98 {
+    private long min=Long.MIN_VALUE;
+    public boolean isValidBST(TreeNode root) {
+        if(root==null) {
+            return true;
+        }
+        boolean left=isValidBST(root.left);
+        if(root.val<=min){
+            return false;
+        }
+        min=root.val;
+        boolean right=isValidBST(root.right);
+        return left&&right;
+    }
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
