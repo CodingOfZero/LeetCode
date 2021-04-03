e9e00dc32971f1a258cc5e98b01b67d6227d323d
hu <895756412@qq.com>
2021-04-04 00:18:54 +0800

    分割回文串，采用动态规划优化回文判断，分割回文dfs与组合是一样的

--- Backtrack/PalindromePartitioning_131.java
+++ Backtrack/PalindromePartitioning_131.java
@@ -1,5 +1,6 @@
 package Backtrack;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -8,41 +9,83 @@ import java.util.List;
  * 将字符串分割成一些子串，使得每个子串都是回文串
  */
 public class PalindromePartitioning_131 {
+//    public List<List<String>> partition(String s){
+//        List<List<String>> res=new LinkedList<>();
+//        if(s==null||s.isEmpty()){
+//            return res;
+//        }
+//        LinkedList<String> cur=new LinkedList<>();
+//
+//        char[] chars = s.toCharArray();
+//        partitionUtilFaster(chars,0,chars.length,res,cur);
+//        return res;
+//    }
+//
+//    private void partitionUtilFaster(char[] chars, int start, int end, List<List<String>> res, LinkedList<String> cur) {
+//        if(start==end){
+//            res.add(new LinkedList<>(cur));
+//            return;
+//        }
+//        for(int i=start;i<end;i++){
+//            if(!check(chars,start,i)){
+//                continue;
+//            }
+//            cur.addLast(new String(chars,start,i-start+1));
+//            partitionUtilFaster(chars,i+1,end,res,cur);
+//            cur.removeLast();
+//        }
+//    }
+//
+//    private boolean check(char[] chars, int start, int end) {
+//        while (start<end){
+//            if(chars[start]!=chars[end]){
+//                return false;
+//            }
+//            start++;
+//            end--;
+//        }
+//        return true;
+//    }
+
     public List<List<String>> partition(String s){
         List<List<String>> res=new LinkedList<>();
         if(s==null||s.isEmpty()){
             return res;
         }
         LinkedList<String> cur=new LinkedList<>();
+        //利用动态规划进行预处理，可以O(1)时间判定字符串是否为回文串
+        int n = s.length();
+        boolean[][] dp=new boolean[n][n];
+        for(boolean[] item:dp){
+            Arrays.fill(item,true);
+        }
+        for(int i=n-1;i>=0;i--){
+            for(int j=i+1;j<n;j++){
+                dp[i][j]=dp[i+1][j-1]&&s.charAt(i)==s.charAt(j);
+            }
+        }
 
-        char[] chars = s.toCharArray();
-        partitionUtilFaster(chars,0,chars.length,res,cur);
+        dfs(s,0,n,res,cur,dp);
         return res;
     }
 
-    private void partitionUtilFaster(char[] chars, int start, int end, List<List<String>> res, LinkedList<String> cur) {
-        if(start==end){
+    private void dfs(String s, int i, int n, List<List<String>> res,
+                     LinkedList<String> cur,boolean[][] dp) {
+        if(i==n){
             res.add(new LinkedList<>(cur));
             return;
         }
-        for(int i=start;i<end;i++){
-            if(!check(chars,start,i)){
-                continue;
+        for(int j=i;j<n;j++){
+            if(dp[i][j]){
+                cur.add(s.substring(i,j+1));
+                dfs(s,j+1,n,res,cur,dp);
+                cur.removeLast();
             }
-            cur.addLast(new String(chars,start,i-start+1));
-            partitionUtilFaster(chars,i+1,end,res,cur);
-            cur.removeLast();
         }
     }
 
-    private boolean check(char[] chars, int start, int end) {
-        while (start<end){
-            if(chars[start]!=chars[end]){
-                return false;
-            }
-            start++;
-            end--;
-        }
-        return true;
+    public static void main(String[] args) {
+        PalindromePartitioning_131 test = new PalindromePartitioning_131();
+        test.partition("aab");
     }
 }
