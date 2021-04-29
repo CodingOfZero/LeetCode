bceee9afb3adfe7444bb809837efe9ee3b757935
hu <895756412@qq.com>
2021-04-29 21:18:22 +0800

    438题与76题同样思路

--- SlidingWindow/FindAllAnagramsinaString_438.java
+++ SlidingWindow/FindAllAnagramsinaString_438.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 /**
  * 找到字符串中所有字母异位词，异位词指字母相同，排列不同的字符串
+ * 与76题同样思路
  */
 public class FindAllAnagramsinaString_438 {
     public List<Integer> findAnagrams(String s, String p) {
