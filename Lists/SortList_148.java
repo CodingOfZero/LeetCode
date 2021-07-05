312eacb25080aeee82b14de24e5c5e4f83209a18
hu <895756412@qq.com>
2021-07-05 09:24:37 +0800

    排序链表，增加时间复杂度为O(nlogn) 空间为O(1)的解法

--- Lists/SortList_148.java
+++ Lists/SortList_148.java
@@ -5,7 +5,7 @@ package Lists;
  * 思路：归并排序
  */
 public class SortList_148 {
-    public ListNode sortList(ListNode head) {
+    /*public ListNode sortList(ListNode head) {
         if(head==null||head.next==null) return head;//特殊情况
         int len=0;
         ListNode p=head;
@@ -53,5 +53,105 @@ public class SortList_148 {
             q=q.next;
         }
         return Dummy.next;
+    }*/
+
+    /**
+     * 自顶向下，递归版的归并排序
+     * 时间复杂度为O(nlogn)，空间为O(logn)
+     */
+    public ListNode sortList(ListNode head) {
+        return sortListHelper(head,null);
     }
+    private ListNode sortListHelper(ListNode head,ListNode tail){
+        if(head==null) return null;
+        //只有一个节点
+        if(head.next==tail){
+            head.next=null;
+            return head;
+        }
+        ListNode fast=head,slow=head;
+        while(fast!=tail){
+            slow=slow.next;
+            fast=fast.next;
+            if(fast!=tail){
+                fast=fast.next;
+            }
+        }
+        ListNode mid=slow;
+        ListNode left=sortListHelper(head,mid);
+        ListNode right=sortListHelper(mid,tail);
+        return merge(left,right);
+    }
+    //合并两个有序链表
+    private ListNode merge(ListNode p,ListNode q){
+        ListNode dummy=new ListNode();
+        ListNode cur=dummy;
+        while(p!=null&&q!=null){
+            ListNode tmp;
+            if(p.val<=q.val){
+                tmp=p;
+                p=p.next;
+            }else{
+                tmp=q;
+                q=q.next;
+            }
+            tmp.next=null;
+            cur.next=tmp;
+            cur=cur.next;
+        }
+        if(p!=null){
+            cur.next=p;
+        }
+        if(q!=null){
+            cur.next=q;
+        }
+        return dummy.next;
+    }
+
+    /**
+     * 以sublen为排序链表的长度,每段长度为sublen，最后一段小于等于sublen
+     * 时间复杂度为O(nlogn) 空间为O(1)
+     */
+    public ListNode sortListLessSpace(ListNode head) {
+        if(head==null||head.next==null) return head;
+        int len=0;
+        ListNode p=head;
+        while(p!=null){
+            len++;
+            p=p.next;
+        }
+        ListNode dummy=new ListNode();
+        dummy.next=head;
+        for(int subLen=1;subLen<len;subLen=subLen*2){
+            ListNode curr=dummy.next;
+            ListNode prev=dummy;
+            while (curr!=null){
+                ListNode head1=curr;
+                for(int i=1;i<subLen&&curr.next!=null;i++){
+                    curr=curr.next;
+                }
+
+                ListNode head2=curr.next;
+                curr.next=null;
+                curr=head2;
+                for(int i=1;i<subLen&&curr!=null&&curr.next!=null;i++){
+                    curr=curr.next;
+                }
+                ListNode next=null;
+                if(curr!=null){
+                    next=curr.next;
+                    curr.next=null;
+                }
+                //待处理
+                ListNode sortedList=merge(head1,head2);
+                prev.next=sortedList;
+                while(prev.next!=null){
+                    prev=prev.next;
+                }
+                curr=next;
+            }
+        }
+        return dummy.next;
+    }
+
 }
